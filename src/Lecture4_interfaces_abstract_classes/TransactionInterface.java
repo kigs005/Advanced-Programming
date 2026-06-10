9473a49ccd3ef11bea27b716f3921ed577a3d8aa
kigs005 <jonnykigs05@gmail.com>
2026-06-10 10:39:45 +0300

    Add methods to TransactionInterface for transaction details and application

--- src/Lecture4_interfaces_abstract_classes/TransactionInterface.java
+++ src/Lecture4_interfaces_abstract_classes/TransactionInterface.java
@@ -16,6 +16,12 @@ public interface TransactionInterface {
     // Method to get a unique identifier for the transaction
     String getTransactionID();
 
+    // Method to print details of the transaction
+    void printTransactionDetails();
+
+    // Method to apply this transaction on a Bank account object
+    void apply(BankAccount ba) throws InsufficientFundsException;
+
 }
 
 
